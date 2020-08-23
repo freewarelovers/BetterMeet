e603cdbfa8362a31f55380db8f5f26e970c5fc27
abderrahmaneMustapha <abderrahmanemustapha030898@gmail.com>
2020-08-23 23:32:04 +0100

    add event to admin site

--- event/models.py
+++ event/models.py
@@ -7,7 +7,7 @@ class Event(models.Model):
     name = models.CharField(_('event name'),max_length=500 )
     slug = models.SlugField(_('event slug'))
     event_creator  = models.ForeignKey(CommunityOwner, verbose_name=_('event creator'),null=True, blank=False, on_delete=models.CASCADE)
-    description = models.TextField(max_length=300)    
+    description = models.TextField(max_length=300, null=True, blank=False)    
     postion = models.CharField('event place,  city or country',max_length=200)
     start_at = models.DateField(_('event starting date'))
     created_at = models.DateTimeField(auto_now_add=True)
