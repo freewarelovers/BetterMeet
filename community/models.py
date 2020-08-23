6f662b921ba8dba3546ae48b865ceaa5d0355561
abderrahmaneMustapha <abderrahmanemustapha030898@gmail.com>
2020-08-23 21:02:57 +0100

    add comunity model

--- community/models.py
+++ community/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
+
+from base.models import Tag
 # Create your models here.
+
+
+class Community(models.Model):
+    name = models.CharField(_('community name'), max_length=200)
+    slug = models.SlugField(_('community slug'))
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class CommunityOwner(models.Model):
+     pass
