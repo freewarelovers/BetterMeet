12f6ac29ac8f586c1a6fadd2e5e1b59d74335054
abderrahmaneMustapha <abderrahmanemustapha030898@gmail.com>
2020-08-27 22:04:51 +0100

    add basic pages and routes to react app

---
+++ frontend/src/domain/registration/login/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+class Login extends React.Component{
+    renderr(){
+        return(
+            <div>Login</div>
+        )
+    }
+}
+
+export default Login
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+
+import Home from './domain/home/index'
+import Login from "./domain/registration/login/index"
+import Signup from "./domain/registration/signin/index"
+
 import * as serviceWorker from './serviceWorker';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useParams
+} from "react-router-dom";
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/signin">
+        <Login/>
+      </Route>
+      <Route path="/signup">
+        <Signup />
+      </Route>
+    </Switch>
+  </Router>,
   document.getElementById('root')
 );
 
