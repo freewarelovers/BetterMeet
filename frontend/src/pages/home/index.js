dfb92ffe8a5d630c33afc10a47eb82c752e53900
abderrahmaneMustapha <abderrahmanemustapha030898@gmail.com>
2020-08-31 21:54:27 +0100

    add a basic files for frontendapp

---
+++ frontend/src/pages/home/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+class Home extends React.Component{
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return(
+           <div> HOme page</div>
+        )
+    }
+}
+export default Home
