326d6cd1966fe8e1dcc593ebc7ed3b97ef477201
abderrahmaneMustapha <abderrahmanemustapha030898@gmail.com>
2020-09-01 20:49:43 +0100

    update

---
+++ frontend/src/components/forms/signupForm/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+import { Formik, Form, Field } from 'formik';
+
+class SignupForm extends React.Component{
+
+    render(){
+        return(
+            <div></div>
+        )
+    }
+}
